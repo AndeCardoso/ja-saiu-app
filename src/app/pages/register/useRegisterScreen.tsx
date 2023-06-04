768117b71540b75c5930263d835e2af1ecac0ff8
AndeCardoso <andersoncardoso.dev@gmail.com>
2023-06-03 21:52:50 -0300

    feat: Add register form validation schema

--- src/app/pages/register/useRegisterScreen.tsx
+++ src/app/pages/register/useRegisterScreen.tsx
@@ -1,5 +1,7 @@
 import { ToastAndroid } from 'react-native';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { useAuth } from 'app/hooks/useAuth';
 import { useNavigation } from '@react-navigation/native';
@@ -11,6 +13,29 @@ export interface FormInputs {
   passwordConfirm: string;
 }
 
+const registerDataSchema = yup.object().shape(
+  {
+    userName: yup.string().min(4, 'É necessario mais que 4 caracteres').required('Este campo é obrigatório'),
+
+    password: yup
+      .string()
+      .required('Este campo é obrigatório')
+      .min(4, 'É necessario mais que 4 caracteres')
+      .when('passwordConfirm', (passwordConfirm, field) =>
+        passwordConfirm ? field.oneOf([yup.ref('passwordConfirm')], 'As senhas informados estão diferentes') : field,
+      ),
+
+    passwordConfirm: yup
+      .string()
+      .required('Este campo é obrigatório')
+      .min(4, 'É necessario mais que 4 caracteres')
+      .when('password', (password, field) =>
+        password ? field.oneOf([yup.ref('password')], 'As senhas informados estão diferentes') : field,
+      ),
+  },
+  [['password', 'passwordConfirm']],
+);
+
 export const useRegisterScreen = () => {
   const { handleUserRegister } = useAuth();
   const { replace, goBack, canGoBack } = useNavigation<any>();
@@ -20,6 +45,8 @@ export const useRegisterScreen = () => {
       password: '',
       passwordConfirm: '',
     },
+    mode: 'onTouched',
+    resolver: yupResolver(registerDataSchema),
   });
 
   const onSubmit = async () => {
