f6752d680101adc7f0bb46c664620fc417e4d2cd
AndeCardoso <andersoncardoso.dev@gmail.com>
2023-09-09 10:04:29 -0300

    fix: Fixing new import due rn-paper update

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,8 +1,8 @@
 import { paperTheme } from '@global/theme/theme';
 import React, { PropsWithChildren } from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import { PaperProvider } from 'react-native-paper';
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
