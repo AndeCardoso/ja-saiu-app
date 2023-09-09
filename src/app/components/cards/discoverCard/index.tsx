077b726d08dd7305d09b858f44df029e478e612b
AndeCardoso <andersoncardoso.dev@gmail.com>
2023-09-09 20:07:10 -0300

    feat: Adding discovery card component

---
+++ src/app/components/cards/discoverCard/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container, Details, StyledImage, Wrapper } from './styles';
+import { Text } from '@components/base/text';
+import { ImageSourcePropType, TouchableOpacity } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { Row } from '@components/base/row';
+
+interface ICardProps {
+  image: ImageSourcePropType;
+  title: string;
+  description: string;
+  episode: string;
+  season: string;
+  favorited: boolean;
+}
+
+export const Card = ({ image, title, description, episode, season, favorited }: ICardProps) => {
+  const { colors } = useTheme();
+  return (
+    <TouchableOpacity>
+      <Container bgColor={colors.secondary} favorited={favorited} favoritedColor={colors.primary}>
+        <StyledImage source={image} resizeMode="cover" size={90} />
+        <Wrapper>
+          <Text fontSize={20} bold={700}>
+            {title}
+          </Text>
+          <Text fontSize={12} textLimit={60}>
+            {description}
+          </Text>
+          <Details>
+            <Row>
+              <Text fontSize={14}>Temp:</Text>
+              <Text fontSize={14} bold={700}>
+                {episode}
+              </Text>
+            </Row>
+            <Row>
+              <Text fontSize={14}>Ep:</Text>
+              <Text fontSize={14} bold={700}>
+                {season}
+              </Text>
+            </Row>
+          </Details>
+        </Wrapper>
+      </Container>
+    </TouchableOpacity>
+  );
+};
---
+++ src/app/components/cards/discoverCard/styles.ts
@@ -0,0 +1,43 @@
+import { Row } from '@components/base/row';
+import styled from 'styled-components/native';
+
+interface IStyledImageProps {
+  size: number;
+}
+
+interface IContainerProps {
+  bgColor: string;
+  favoritedColor: string;
+  favorited: boolean;
+}
+
+export const Container = styled(Row)<IContainerProps>`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 5px;
+  border-top-left-radius: 52px;
+  border-bottom-left-radius: 52px;
+  border-right-color: ${({ favoritedColor, bgColor, favorited }) => (favorited ? favoritedColor : bgColor)};
+  border-right-width: 8px;
+  border-right-style: solid;
+  margin: 8px 0;
+  padding: 8px;
+  background-color: ${({ bgColor }) => bgColor};
+`;
+
+export const Wrapper = styled.View`
+  flex: 1;
+  gap: 8px;
+  margin-left: 16px;
+`;
+
+export const Details = styled(Row)`
+  gap: 16px;
+`;
+
+export const StyledImage = styled.Image<IStyledImageProps>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-radius: 100px;
+`;
